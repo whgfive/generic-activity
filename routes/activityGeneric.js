3f81b3eaf50fe219f49d11bd9396e5332345455c
William Garrison <wgarrison@wgarrison-ltm.internal.salesforce.com>
2016-03-28 12:41:15 -0700

    test

--- routes/activityGeneric.js
+++ routes/activityGeneric.js
@@ -3,36 +3,36 @@ var https = require('https');
 var activityUtils = require('./activityUtils');
 
 exports.edit = function(req, res) {
-    console.log("Header:"+req.headers);
-    console.log("Body:"+req.body);
+    console.log("Header:"+JSON.stringify(req.headers));
+    console.log("Body:"+JSON.stringify(req.body));
     activityUtils.logData(req);
     res.send(200, 'Edit');
 };
 
 exports.save = function(req, res) {
-    console.log("Header:"+req.headers);
-    console.log("Body:"+req.body);
+    console.log("Header:"+JSON.stringify(req.headers));
+    console.log("Body:"+JSON.stringify(req.body));
     activityUtils.logData(req);
     res.send(200, 'Save');
 };
 
 exports.publish = function(req, res) {
-    console.log("Header:"+req.headers);
-    console.log("Body:"+req.body);
+    console.log("Header:"+JSON.stringify(req.headers));
+    console.log("Body:"+JSON.stringify(req.body));
     activityUtils.logData(req);
     res.send(200, 'Publish');
 };
 
 exports.validate = function(req, res) {
-    console.log("Header:"+req.headers);
-    console.log("Body:"+req.body);
-    activityUtils.logData(req.body);
+    console.log("Header:"+JSON.stringify(req.headers));
+    console.log("Body:"+JSON.stringify(req.body));
+    activityUtils.logData(JSON.stringify(req.body));
     res.send(200, 'Validate');
 };
 
 exports.execute = function(req, res) {
-    console.log("Header:"+req.headers);
-    console.log("Body:"+req.body);
-    activityUtils.logData(req.body);
+    console.log("Header:"+JSON.stringify(req.headers));
+    console.log("Body:"+JSON.stringify(req.body));
+    activityUtils.logData(JSON.stringify(req.body));
     res.send(200, 'Execute');
 };
